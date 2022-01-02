3c3f63a247638868711a8710cc0aafe3e004dc8e
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-02 17:49:00 -0500

    updated install

--- easy-install-rpi-aarch64.sh
+++ easy-install-rpi-aarch64.sh
@@ -5,15 +5,15 @@ set -eu
 echo "Starting install of V4L2 to NDI for Raspberry Pi 4 64-bit...\n"
 
 echo "Installing prerequisites...\n"
-sudo ./preinstall.sh
+sudo bash ./preinstall.sh
 
 echo "Downloading NDI libraries...\n"
-sudo ./download_NDI_SDK.sh
+sudo bash ./download_NDI_SDK.sh
 
 echo "Building executable for Raspberry Pi 4 64-bit...\n"
-sudo ./build_rpi4_arm64.sh
+sudo bash ./build_rpi4_arm64.sh
 
 echo "Installing in final directory...\n"
-sudo ./install.sh
+sudo bash ./install.sh
 
 echo "Done"
