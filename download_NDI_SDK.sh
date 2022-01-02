e8ce6ac8d0a93b2dd72d5a3cbd9d8aaf154e6052
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-02 17:18:53 -0500

    added download option for NDI

---
+++ download_NDI_SDK.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+#download and extract NDI
+curl -s https://downloads.ndi.tv/SDK/NDI_SDK_Linux/Install_NDI_SDK_v5_Linux.tar.gz | tar xvz -C /tmp/
+yes y | bash /tmp/Install_NDI_SDK_v5_Linux.sh > /dev/null
