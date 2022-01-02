26b177098deff660509043a574858609f5dc5d3a
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-02 17:53:44 -0500

    added error management

--- install.sh
+++ install.sh
@@ -4,9 +4,17 @@ INSTALL_DIR="/opt/v4l2ndi"
 BIN_DIR="$INSTALL_DIR/bin"
 LIB_DIR="$INSTALL_DIR/lib"
 
-mkdir "$INSTALL_DIR"
-mkdir "$LIB_DIR"
-mkdir "$BIN_DIR"
+if [ ! -d "$INSTALL_DIR" ]; then
+  mkdir "$INSTALL_DIR"
+fi
+
+if [ ! -d "$LIB_DIR" ]; then
+  mkdir "$LIB_DIR"
+fi
+
+if [ ! -d "$BIN_DIR" ]; then
+  mkdir "$BIN_DIR"
+fi
 
 cp lib/* "$LIB_DIR"
 
